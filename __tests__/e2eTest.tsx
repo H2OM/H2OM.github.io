a3bbf7f59b88b99eba956f3e4ba04f423913e8f9
Dmitrii <dima.zabolotnov.02@mail.ru>
2024-08-23 16:59:40 +0300

    Clear gh-pages branch

--- __tests__/e2eTest.tsx
+++
@@ -1,8 +0,0 @@
-import Router from "../src/Router";
-import {render} from "@testing-library/react";
-
-describe('e2eTest', ()=>{
-    it('fullTest', async ()=>{
-        render(<Router/>);
-    });
-});
